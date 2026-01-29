5d97282a84c947338490e2c184952826d9589e54
Hemant Kumar Sah <sah20080116@gmail.com>
2026-01-29 22:23:20 +0530

    done

--- list.py
+++ list.py
@@ -1,5 +1,5 @@
 x = [5,"Hello",3.4,False,0,True,"World",5j]
-x.append("New Element")
+"""x.append("New Element")
 print(x)
 
 del x[2]
@@ -7,3 +7,43 @@ del x[2]
 print(x)
 
 print(x[-1])
+
+for i in range(2,len(x)):
+    print(x[i])
+
+print(x)    
+print(x)
+#for changing list elements
+
+x[:4] = "Changed Element","hi",5,6.7,True
+
+print(x)
+
+
+x.append("New Element")
+print(x)
+
+
+
+
+x.insert(1,"Tried insert")
+
+print(x) 
+y = [5,30]
+x.extend(y)
+
+print(x) 
+
+x.remove(5) 
+
+print(x)"""
+
+"""x.pop(0)  
+print(x)"""
+
+
+
+    i=0
+while i < len(x):
+    print(x)
+    i += 1
